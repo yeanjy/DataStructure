a00679496a6225e61570979408ae2b6e630bdff9
Yean Jy Chen <yeanchen3@gmail.com>
2024-02-26 13:48:07 -0300

    add doublelinkedlist

---
+++ linkedlist/doublelinkedlist.c
@@ -0,0 +1,97 @@
+#include <assert.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node {
+  int val;
+  struct node *next;
+  struct node *previus;
+} node;
+
+typedef struct linkedlist {
+  int size;
+  node *head;
+} linkedlist;
+
+void init(linkedlist **l) {
+  assert(l != NULL && *l != NULL);
+
+  *l = malloc(sizeof(linkedlist));
+  (*l)->head = NULL;
+  (*l)->size = 0;
+}
+
+void insert(linkedlist **l, int val) {
+  assert(l != NULL && *l != NULL);
+
+  node *temp = malloc(sizeof(node));
+  temp->val = val;
+  temp->next = NULL;
+  temp->previus = NULL;
+
+  if ((*l)->head == NULL) {
+    (*l)->head = temp;
+    (*l)->size = 1;
+  } else {
+    node *current = (*l)->head;
+    while (current->next != NULL) {
+      current = current->next;
+    }
+
+    current->next = temp;
+    temp->previus = current;
+    (*l)->size++;
+  }
+}
+
+void printList(linkedlist *l) {
+  assert(l != NULL && l->head != NULL);
+
+  node *current = l->head;
+  while (current != NULL) {
+    printf("current val number: %d\n", current->val);
+    if (current->previus != NULL) {
+      printf("Previous val number: %d\n", current->previus->val);
+    } else {
+      printf("Previous val: NULL\n");
+    }
+    if (current->next != NULL) {
+      printf("Next val number: %d\n", current->next->val);
+    } else {
+      printf("Next val: NULL\n");
+    }
+    printf("\n");
+    current = current->next;
+  }
+}
+
+void deletList(linkedlist **l) {
+  assert(l != NULL && *l != NULL && (*l)->head != NULL);
+
+  if ((*l)->head == NULL) {
+    free(*l);
+    *l = NULL;
+    return;
+  }
+
+  node *temp;
+  while ((*l)->head != NULL) {
+    temp = (*l)->head->next;
+    free((*l)->head);
+    (*l)->head = temp;
+  }
+  free(*l);
+  *l = NULL;
+}
+
+int main() {
+  linkedlist *l;
+  init(&l);
+  insert(&l, 1);
+  insert(&l, 2);
+  insert(&l, 3);
+  printList(l);
+  deletList(&l);
+  return 0;
+}
